792d00edceece310b5fa6797c4674ff8af275ef3
Gabor Csikos <csiki123@gmail.com>
2015-10-21 21:34:50 +0200

    added Main

---
+++ cs50-final-project/resolution/src/main/java/backend/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package backend;
+
+/**
+ * @author Gabor Csikos
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
